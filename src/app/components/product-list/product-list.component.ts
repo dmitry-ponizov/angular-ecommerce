6037e708d432d5eb94ffb4cb7512705f530db6ac
Dmitry Ponisov <dmitry.ponisov@sanecum.io>
2026-01-26 21:05:28 +0100

    first commit

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -15,7 +15,12 @@ export class ProductListComponent implements OnInit {
   products: Product[] = [];
 
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   searchMode: boolean = false;
+
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElements: number = 0;
 
   constructor(
     private productService: ProductService,
@@ -54,10 +59,21 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryId = 1;
     }
 
+    if(this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    } 
+
+    this.previousCategoryId = this.currentCategoryId;
+
+
     this.productService
-      .getProductList(this.currentCategoryId)
-      .subscribe((data) => {
-        this.products = data;
-      });
+      .getProductListPaginate(this.thePageNumber - 1, this.thePageSize, this.currentCategoryId).subscribe(
+        data => {
+          this.products = data._embedded.products;
+          this.thePageNumber = data.page.number + 1;
+          this.thePageSize = data.page.size;
+          this.theTotalElements = data.page.totalElements;
+        }
+      );
   }
 }
